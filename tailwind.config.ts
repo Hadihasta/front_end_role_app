792736ecac0dee2218fa455a20c15c576a3c5899
hadiasta <hijrihadi@gmail.com>
2025-10-12 17:42:40 +0700

    feat : seprate file for great structure add shadcn component to the project add tailwind configuration v.3 since v.4 not stable

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f172a",
+        secondary: "#38bdf8",
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
