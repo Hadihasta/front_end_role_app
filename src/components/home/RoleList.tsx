1ec9697229f21e91c3608fb7d18d9e1ffde27676
hadiasta <hijrihadi@gmail.com>
2025-10-13 06:29:05 +0700

    add axios instance and fetch api get roles but issue cors not allowed 3000 to 8080/roles

--- src/components/home/RoleList.tsx
+++ src/components/home/RoleList.tsx
@@ -1,16 +1,40 @@
+'use client'
 import React from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from '@/lib/axios'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 
 const RoleList = () => {
-  const tags = Array.from({ length: 4 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    try {
+      // console.log('Fetching data from API...')
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}roles`
+        // ,{
+        //   headers: { Authorization: `Bearer ${token}` },
+        // }
+      )
+      // setData(res.data)
+      console.log(res)
+    } catch (error) {
+      console.error('Gagal mengambil data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [data])
+
+  const tags = Array.from({ length: 10 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
 
   return (
     <div>
       <ScrollArea className="h-72 w-150 rounded-md border">
         <div className="p-4">
-          <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+          <h4 className="mb-4 text-xl leading-none font-medium">Roles</h4>
           {tags.map((tag) => (
             <React.Fragment key={tag}>
               <div className="text-sm">{tag}</div>
