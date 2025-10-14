44a661dabbe2eb5bd8165d3202b7f529a3c2491b
hadiasta <hijrihadi@gmail.com>
2025-10-14 09:38:00 +0700

    feat : add skeleton shadcn move fetching to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,55 @@
 'use client'
 import FormField from '@/components/home/FormField'
 import dynamic from 'next/dynamic'
-import Skeleton from '@/components/global/Skeleton'
+import SkeletonTemplate from '@/components/global/SkeletonTemplate'
+import { useEffect, useState } from 'react'
+import axios from '@/lib/axios'
 
 //disable eslint for the next line
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-const RoleList = dynamic(() => 
-  new Promise<{ default: React.ComponentType<any> }>((resolve) => {
-    setTimeout(() => {
-      resolve(import('@/components/home/RoleList'))
-    }, 2000)
-  }),
+const RoleList = dynamic(
+  () =>
+    new Promise<{ default: React.ComponentType<any> }>((resolve) => {
+      setTimeout(() => {
+        resolve(import('@/components/home/RoleList'))
+      }, 2000)
+    }),
   {
-    loading: () => <Skeleton />,
+    loading: () => <SkeletonTemplate />,
     ssr: false,
   }
 )
 /* eslint-enable @typescript-eslint/no-explicit-any */
 
+interface Role {
+  id: number
+  role_name: string
+}
+
+
 export default function Home() {
+ const [roleMaster, setroleMaster] = useState<Role[]>([])
+
+   const fetchData = async () => {
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/roles`)
+      setroleMaster(res.data)
+      console.log(res)
+    } catch (error) {
+      console.error('Gagal mengambil data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
   return (
     <>
       <div className=" flex flex-col font-sans items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-        
-
-        <RoleList />
+        <RoleList  data={roleMaster}/>
         <FormField />
       </div>
     </>
--- src/components/home/RoleList.tsx
+++ src/components/home/RoleList.tsx
@@ -1,47 +1,34 @@
 'use client'
 import React from 'react'
-import { useEffect, useState, Suspense } from 'react'
-import axios from '@/lib/axios'
+
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
-
-
 
 interface Role {
   id: number
   role_name: string
 }
 
-const RoleList = () => {
-  const [dataMaster, setDataMaster] = useState<Role[]>([])
+interface RoleListProps {
+  data: Role[]
+}
 
-  const fetchData = async () => {
-    try {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/roles`)
-      setDataMaster(res.data)
-      console.log(res)
-    } catch (error) {
-      console.error('Gagal mengambil data:', error)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+const RoleList = ({ data }: RoleListProps) => {
   return (
+    // if props is objct or array use JSON.stringify to display it
+    // or use map if array of object
 
-      <ScrollArea className="h-72 w-150 rounded-md border">
-        <div className="p-4">
-          <h4 className="mb-4 text-xl leading-none font-medium">Roles</h4>
-          {dataMaster.map((data, index) => (
-            <React.Fragment key={index}>
-              <div className="text-sm">{data.role_name} </div>
-              <Separator className="my-2" />
-            </React.Fragment>
-          ))}
-        </div>
-      </ScrollArea>
+    <ScrollArea className="h-72 w-150 rounded-md border">
+      <div className="p-4">
+        <h4 className="mb-4 text-xl leading-none font-medium">Roles</h4>
+        {data.map((data, index) => (
+          <React.Fragment key={index}>
+            <div className="text-sm">{data.role_name} </div>
+            <Separator className="my-2" />
+          </React.Fragment>
+        ))}
+      </div>
+    </ScrollArea>
   )
 }
 
