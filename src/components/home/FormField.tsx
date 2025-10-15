f6b12c0c1d151ab0beebc78aa2fbcf832652aa2e
hadiasta <hijrihadi@gmail.com>
2025-10-15 10:47:26 +0700

    feat : complete payload for send request to roles

--- src/components/home/FormField.tsx
+++ src/components/home/FormField.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import axios from '@/lib/axios'
 
 import {
   Field,
@@ -12,10 +14,33 @@ import {
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
 
+
+const sendFormData = async (role: string) => {
+  try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/role`, { role_name: role })
+      console.log('Response:', res.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const FormField = () => {
+  const [role, setRole] = useState<string>('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault() // prevent page reload
+    sendFormData(role)
+    
+    console.log('Submitted role:', role)
+
+    // you can also do something with the value here:
+    // e.g. send to backend or dispatch Redux action
+    setRole('') // clear the input after submit
+  }
+
   return (
     <div className="w-full max-w-md">
-      <form>
+      <form onSubmit={handleSubmit}>
         <FieldGroup>
           <FieldSet>
             <FieldLegend>Add To List</FieldLegend>
@@ -23,9 +48,12 @@ const FormField = () => {
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="checkout-7j9-card-name-43j">Role</FieldLabel>
-                <Input
-                  id="checkout-7j9-card-name-43j"
+                   <Input
+                  id="role-input"
+                  name="role"
                   placeholder="Input Role here..."
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
                   required
                 />
               </Field>
@@ -34,10 +62,11 @@ const FormField = () => {
           <FieldSeparator />
 
           <Field orientation="horizontal">
-            <Button type="submit">Submit</Button>
+               <Button type="submit">Submit</Button>
             <Button
               variant="outline"
               type="button"
+              onClick={() => setRole('')}
             >
               Cancel
             </Button>
